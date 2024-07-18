9f43ec3cf7874fc11d89ba8a0a71d8a1e6f4879e
ntnyq <ntnyq13@gmail.com>
2024-07-19 02:36:14 +0800

    chore: update

--- build.config.ts
+++ build.config.ts
@@ -2,8 +2,8 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: ['src/index'],
-  declaration: true,
   clean: true,
+  declaration: true,
   externals: ['dayjs'],
   rollup: {
     emitCJS: true,
